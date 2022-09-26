2e6fbf181c9c26daca93e9ee32ff483eb8d639d8
j.f <usausa13108891@gmail.com>
2022-09-26 21:17:51 +0000

    for heroku

--- db/migrate/20220704102640_create_bokes.rb
+++ db/migrate/20220704102640_create_bokes.rb
@@ -3,9 +3,6 @@ class CreateBokes < ActiveRecord::Migration[6.1]
     create_table :bokes do |t|
       t.string :boke
       t.string :image_url
-
-      t.timestamps TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      t.created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
     end
   end
 end
